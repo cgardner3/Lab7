b1342308364acfe8d5842c8a5ea04120ddb39f31
Cheryl Gardner <cgardner3@hawk.iit.edu>
2020-04-20 15:19:38 -0500

    Updates

--- js/script.js
+++ js/script.js
@@ -1,10 +1,20 @@
 function init(){
-	var button = document.getElementById('entrybutton');
-	var message = document.getElementById('entryinput').value;
-	var display = document.getElementById('textoutput')
-	button.addEventListener('click',fuction() {
-		alert("Cheryl Gardner: " + message);
-		display.innerHTML = message.value;
-	});
+
+var button = document.getElementById('entrybutton');
+var message = document.getElementById('entryinput')
+var display = document.getElementById('textoutput')
+var name = document.getElementsByClassName('center')[1];
+
+function alertFunction(){
+	alert(name.textContent + ': ' + message.value);
 }
+
+function outputFunction(){
+	display.innerHTML = message.value;
+}
+
+button.addEventListener('click', alertFunction);
+button.addEventListener('click', outputFunction);
+}
+
 window.addEventListener('load', init);
